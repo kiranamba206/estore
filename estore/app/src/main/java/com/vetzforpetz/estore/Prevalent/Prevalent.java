52b9e5f5d9f2db2fbdeda491198b91a113c7b2a9
kiranamba206 <kiranamba206@gmail.com>
2020-03-25 19:57:10 -0700

    Create Prevalent.java

---
+++ estore/app/src/main/java/com/vetzforpetz/estore/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.vetzforpetz.estore.Prevalent;
+
+import com.vetzforpetz.estore.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
