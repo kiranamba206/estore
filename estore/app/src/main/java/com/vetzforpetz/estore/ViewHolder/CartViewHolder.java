36b782fd59f0ec46097d7b25bc3da8de5f352f36
kiranamba206 <kiranamba206@gmail.com>
2020-03-25 20:04:44 -0700

    Add files via upload

---
+++ estore/app/src/main/java/com/vetzforpetz/estore/ViewHolder/CartViewHolder.java
@@ -0,0 +1,36 @@
+package com.vetzforpetz.estore.ViewHolder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vetzforpetz.estore.Interface.ItemClickListener;
+import com.vetzforpetz.estore.R;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public TextView txtProductName, txtProductPrice, txtProductQuantity;
+    private ItemClickListener itemClickListner;
+
+
+    public CartViewHolder(View itemView)
+    {
+        super(itemView);
+
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        itemClickListner.onClick(view, getAdapterPosition(), false);
+    }
+
+    public void setItemClickListner(ItemClickListener itemClickListner)
+    {
+        this.itemClickListner = itemClickListner;
+    }
+}
